10cc2d65eb92d79b8f39d80b92537ff0ca24c0e9
loex345 <maitlandeg@gmail.com>
2022-09-02 10:45:23 -0700

    Initial commit

---
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -0,0 +1,11 @@
+import { checkToken } from '../../utilities/users-service';
+
+export default function OrderHistoryPage() {
+  
+
+  return (
+    <>
+      <h1>OrderHistoryPage</h1>
+    </>
+  );
+}
