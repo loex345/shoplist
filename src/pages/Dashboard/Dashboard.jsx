1d2a85a391505ca1beb393299c80049c7d2787d2
loex345 <maitlandeg@gmail.com>
2022-10-24 22:10:15 -0700

    Css update

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -5,7 +5,7 @@ import '../Dashboard/Dashboard.css'
 export default function Dashboard({ user, shopItems, lists, setLists}){
     return (
         <main>
-            <section className="py-5 text-center container">
+            <section className="py-5 d-flex justify-content-center text-center container">
                 <div className="album py-5 bg-light-">
                             <div className="row row-cols-1 rows-cols-1">
                             <h1>Welcome to Dashboard</h1>
