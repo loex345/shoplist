0e5434a48a457053dcf0ae9cc3835728e76b7af1
loex345 <maitlandeg@gmail.com>
2022-10-25 21:07:16 -0700

    Css update

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -10,16 +10,16 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <nav className="navbar navbar-expand-lg">
-      <Link to='/list/dashboard'>Dashboard</Link>
+    <nav className="navbar navbar-expand-lg justify-content-center">
+      <Link to='/list/dashboard' className="btn btn-outline-primary">Dashboard</Link>
       &nbsp; | &nbsp;
-      <Link to='/list/newItem'>Add List/Item </Link>
+      <Link to='/list/newItem' className="btn btn-outline-primary">Add List/Item </Link>
       &nbsp; | &nbsp;
-      <Link to='/list/newList'>Add New List</Link>
+      <Link to='/list/newList' className="btn btn-outline-primary">Add New List</Link>
       &nbsp; | &nbsp;
       Welcome, {user.name}
       &nbsp; | &nbsp;
-      <Link to='' onClick={handleLogOut}>Log Out</Link>
+      <Link to='' onClick={handleLogOut} className="btn btn-outline-primary">Log Out</Link>
     </nav>
   );
 }
